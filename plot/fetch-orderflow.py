4406ed538a788039d4b5c885ca6f94f33bcfff84
Jake Adelman <jacobzadelman@gmail.com>
2019-12-28 03:25:23 -0500

    fix 0 err for BPA

---
+++ plot/fetch-orderflow.py
@@ -0,0 +1,113 @@
+import requests
+import json
+from pprint import pprint
+import sqlite3
+import datetime, time
+import psycopg2
+
+
+
+
+class Bitmex(object):
+        
+        def __init__(self):
+                self.now_h = datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M")
+                self.five_mins_ago = datetime.datetime.utcnow()- datetime.timedelta(minutes=5)
+                self.fma_h = self.five_mins_ago.strftime("%Y-%m-%d %H:%M")
+                # print(now_h, fma_h)
+
+                self.BPA = 0
+                self.SPA = 0
+                self.buys = 0
+                self.sells = 0
+                self.buycount = 0
+                self.sellcount = 0
+               
+
+
+        def fetch_open_interest(self):
+                STATS = "https://www.bitmex.com/api/v1" + '/stats'
+
+                r = requests.get(url=STATS)
+
+                data = r.json()
+                for i in data:
+                        if i['rootSymbol']=='XBT':
+                                open_interest = i['openInterest']
+                                return(open_interest)
+
+        def fetch_longs_shorts(self):
+                URL = "https://www.bitmex.com/api/v1" + "/trade"
+                # print(self.now_h)
+                filt = {}
+                PARAMS ={
+                        'symbol':'XBT',
+                        'filter':json.dumps(filt),
+                        'count':1000,
+                        'startTime':self.fma_h,
+                        'endTime':self.now_h
+                        }
+
+                r = requests.get(url=URL, params=PARAMS)
+                # pprint(r.json())
+                data = r.json()
+              
+                
+
+                for dat in data:
+                        if dat['side']=='Buy':
+                                self.buys+=dat['size']
+                                self.BPA+=dat['price']
+                                self.buycount+=1
+                        if dat['side']=='Sell':
+                                self.sells+=dat['size']
+                                self.SPA+=dat['price']
+                                self.sellcount+=1
+                print(self.buys, self.sells, self.BPA, self.SPA, len(data))
+                try:
+                        BPAfin=self.BPA//self.buycount
+                        SPAfin=self.SPA//self.sellcount
+                except:
+                        BPAfin=data[-1]['price']
+                        SPAfin=data[-1]['price']
+                AVGprice=(self.BPA+self.SPA)/(self.buycount+self.sellcount)
+                AVGprice= round(AVGprice,1)
+                print(BPAfin, SPAfin,AVGprice)                
+                long_short = {}
+                long_short['longs'] = self.buys
+                long_short['shorts'] = self.sells
+                long_short['BPA'] = BPAfin
+                long_short['SPA'] = SPAfin
+                long_short['AVGprice']=AVGprice
+                
+                return(long_short)
+                              
+        def run(self):                       
+                OI = self.fetch_open_interest()
+                LS = self.fetch_longs_shorts()
+                
+
+                IQ = """INSERT INTO liquidity
+                        (openinterest, long, short, bpa, spa, avgprice, timestamp)
+                        VALUES
+                        (%s, %s, %s, %s, %s, %s, %s);"""
+
+                VALS = (OI, LS['longs'], LS['shorts'], LS['BPA'], LS['SPA'], LS['AVGprice'], str(self.now_h))
+                print(VALS)
+                conn = psycopg2.connect(host='localhost',
+                                        port="5432",
+                                        database="bitmex", 
+                                        user="jakeadelman", 
+                                        password="jakeadelman")
+                c = conn.cursor()
+                c.execute(IQ, VALS)
+                conn.commit()
+
+# Bitmex().fetch_longs_shorts()
+# Bitmex().run()
+while True:
+        if datetime.datetime.now().minute % 1 == 0:
+                Bitmex().run()
+        time.sleep(60)
+
+#(market) close long + open short : market open long + close short˝
---
+++ plot/plot.py
@@ -0,0 +1,179 @@
+import psycopg2
+import matplotlib.pyplot as plt
+import numpy as np
+
+# def fetch():
+#     conn = psycopg2.connect(host='165.22.62.101',
+#                             port="5432",
+#                             database="bitmex", 
+#                             user="jakeadelman", 
+#                             password="jakeadelman")
+#     c = conn.cursor()
+#     SQL = "select * from liquidity;"
+
+
+#     res = c.execute(SQL)
+#     print(res)
+
+def fetch():
+    conn = None
+    mins = (24*60)/5
+
+    try:
+        conn = psycopg2.connect(host='165.22.62.101',
+                            port="5432",
+                            database="bitmex", 
+                            user="jakeadelman", 
+                            password="jakeadelman")
+        cur = conn.cursor()
+        cur.execute("SELECT id, openinterest, long, short, bpa, spa, avgprice, timestamp FROM liquidity WHERE"+
+        " id>100 ORDER BY id DESC")
+        rows = cur.fetchall()
+        fin = []
+        ids = []
+        openinterest = []
+        longs = []
+        short = []
+        bpa = []
+        spa = []
+        avgprice = []
+        timestamp =[]
+        longshort = 0
+        print("The number of rows: ", cur.rowcount)
+        for row in rows:
+            # print(row)
+            
+            try:
+                OI1 = openinterest[-1]
+                
+                # initchange = row[1]-OI1
+                # longshort = row[2]/row[3]
+                openinterest.append(row[1])
+                ids.append(row[0])
+                # longchange = initchange*longshort
+                # newlong = longs[-1]+longchange
+                # shortchange = initchange*(1-longshort)
+                # newshort = short[-1]+shortchange
+               
+                longs.append(row[2])            
+                short.append(row[3]) 
+                bpa.append(row[4])
+                spa.append(row[5])
+                avgprice.append(row[6])
+                timestamp.append(row[7])           
+            except:
+                # print("out")
+                ids.append(row[0])
+                openinterest.append(row[1]) 
+                longdiv = (row[1]/2)
+                longs.append(longdiv)
+                short.append(longdiv)
+                bpa.append(row[4])
+                spa.append(row[5])
+                avgprice.append(row[6])
+                timestamp.append(row[7])
+            
+        ids.reverse()
+        fin.append(ids)
+        openinterest.reverse()
+        fin.append(openinterest)
+        longs.reverse()
+        fin.append(longs)
+        short.reverse()
+        fin.append(short)
+        bpa.reverse()
+        fin.append(bpa)
+        spa.reverse()
+        fin.append(spa)
+        avgprice.reverse()
+        fin.append(avgprice)
+        timestamp.reverse()
+        fin.append(timestamp)
+        return(fin)
+
+
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def format_long_short(res):
+    final = []
+    openinterest = []
+    newlongs = []
+    newshorts = []
+    initinterest = 0
+    initchange = 0
+    longshort = 0
+    longchange = 0
+    shortchange = 0
+    newlong = 0
+    newshort = 0
+    longshort = 0
+    opens = res[1]
+    longs = res[2]
+    short = res[3]
+
+    i=0
+
+    while i< len(opens):
+        
+        try:
+            
+            OI1 = openinterest[-1]
+
+            initchange = opens[i]-OI1
+            tot = longs[i]+short[i]
+            longshort = longs[i]/tot
+
+            openinterest.append(opens[i])
+            longchange = initchange*longshort
+            newlong = longs[-1]+longchange
+            shortchange = initchange*(1-longshort)
+            newshort = short[-1]+shortchange
+            longs[i]=newlong
+            short[i]=newshort
+            newlongs.append(newlong)            
+            newshorts.append(newshort) 
+            i+=1
+        except:
+            openinterest.append(opens[i])
+            newlongs.append(opens[i]//2)
+            newshorts.append(opens[i]//2)
+            i+=1
+
+    final.append(opens)
+    final.append(newlongs)
+    final.append(newshorts)
+
+    return(final)
+
+initres = fetch()
+res = format_long_short(initres)
+
+
+y = np.array(initres[0])
+x = np.array(initres[1])
+x2 = np.array(res[1])
+x3 = np.array(res[2])
+print("last timestamp: "+ initres[7][-1])
+
+fig, axs = plt.subplots(3)
+# fig.suptitle('Vertically stacked subplots')
+axs[0].plot(y, x)
+axs[1].plot(y, x2)
+axs[1].plot(y, x3)
+axs[2].plot(y, initres[4])
+axs[2].plot(y, initres[5])
+axs[2].plot(y, initres[6])
+
+labels=['openinterest','long/short change','price']
+i=0
+while i<len(axs.flat):
+    axs.flat[i].set(ylabel=labels[i])
+    i+=1
+
+plt.show()
